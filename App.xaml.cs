ec68ea322f0654d778fbd597a0dc0ccadf42e9af
SanTTiaGGo64 <166181745+SanTTiaGGo64@users.noreply.github.com>
2024-05-23 21:36:33 -0500

    Add files via upload

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Maui.Controls;
+
+namespace pryReservas
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            MainPage = new NavigationPage(new Views.login());
+        }
+    }
+}
